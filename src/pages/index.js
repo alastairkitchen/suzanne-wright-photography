1ec665c151179fff3cd4ac0fd225d2045d8d9aca
alastairkitchen <ali7566@gmail.com>
2019-03-08 22:00:30 +0000

    Adds url and cover image fields to gallery, removes images from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import { graphql } from "gatsby";
 import SiteLayout from "../components/layout/siteLayout";
 
-const images = images => {
-  if (images) {
-    return images.map((image, i) => {
-      return <img key={i} src={image} />;
-    });
+const pageListItem = page => {
+  if (page) {
+    return test;
   }
 };
 
@@ -14,13 +12,6 @@ export default ({ data }) => {
   console.dir(data);
   return (
     <SiteLayout>
-      {images(
-        data.allFile !== null
-          ? data.allFile.edges[2].node.childMarkdownRemark.frontmatter
-              .galleryImages
-          : false
-      )}
-
       <div>Homepage</div>
       <img src="https://via.placeholder.com/468x200" />
       <img src="https://via.placeholder.com/468x200" />
@@ -32,25 +23,21 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile(
-      filter: {
-        dir: {
-          regex: "/Applications/MAMP/htdocs/www.suzannewrightphotographer.internal/src/imageGalleries/"
-        }
-      }
+    allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "image-gallery" } } }
     ) {
       edges {
         node {
-          name
-          dir
-          childMarkdownRemark {
-            id
-            html
-            frontmatter {
-              title
-              templateKey
-              galleryImages
-            }
+          id
+          frontmatter {
+            title
+            templateKey
+            date
+            description
+            galleryImages
+            url
+            coverImage
+            _PARENT
           }
         }
       }
