6ab67fa7bb833fe443cac9abcfd8264f8940bfeb
alastairkitchen <ali7566@gmail.com>
2019-04-14 12:13:46 +0100

    Adds commerical page component

---
+++ src/pages/commercial.js
@@ -0,0 +1,57 @@
+import React from "react";
+import SiteLayout from "../components/layout/siteLayout";
+import ImageGallery from "../components/imageGallery/imageGallery";
+
+const Commercial = ({ data }) => {
+  return (
+    <SiteLayout>
+      <ImageGallery {...data} />
+    </SiteLayout>
+  );
+};
+
+export default Commercial;
+
+export const query = graphql`
+  query {
+    allImages: allFile(
+      filter: { extension: { regex: "/(jpg)|(png)|(gif)|(jpeg)/" } }
+    ) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            # Specify the image processing specifications right in the query.
+            fluid(maxWidth: 600, maxHeight: 600) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    galleryImages: allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: { eq: "image-gallery" }
+          title: { eq: "Commercial" }
+        }
+      }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            templateKey
+            date
+            description
+            galleryImages
+            url
+            coverImage
+          }
+        }
+      }
+    }
+  }
+`;
