9293caf0048dbff117fd528f5f50d2eb5adef0d7
alastairkitchen <ali7566@gmail.com>
2020-06-12 21:19:01 +0100

    Adds navigation active

--- src/components/header/navigation.js
+++ src/components/header/navigation.js
@@ -1,31 +1,35 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
 
 const Navigation = (props) => {
 
+  let activePage = window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
+
+  console.dir(activePage)
+
   return (
     <nav>
-      <ol className="navigation">
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/">Home</Link>
+      <ol className='navigation'>
+        <li className={`navigation__list ${window.location.pathname === '/' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/'>Home</Link>
         </li>
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/recent-work">Recent Work</Link>
+        <li className={`navigation__list ${activePage === 'recent-work' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/recent-work'>Recent Work</Link>
         </li>
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/family">Family</Link>
+        <li className={`navigation__list ${activePage === 'family' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/family'>Family</Link>
         </li>
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/portraits">Portraits</Link>
+        <li className={`navigation__list ${activePage === 'portraits' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/portraits'>Portraits</Link>
         </li>
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/weddings">Weddings</Link>
+        <li className={`navigation__list ${activePage === 'weddings' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/weddings'>Weddings</Link>
         </li>
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/about">About</Link>
+        <li className={`navigation__list ${activePage === 'about' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/about'>About</Link>
         </li>
-        <li className="navigation__list">
-          <Link onClick={() => props.closeMenu()} to="/contact">Contact</Link>
+        <li className={`navigation__list ${activePage === 'contact' ? 'navigation__list-active' : ''}`}>
+          <Link onClick={() => props.closeMenu()} to='/contact'>Contact</Link>
         </li>
       </ol>
     </nav>
