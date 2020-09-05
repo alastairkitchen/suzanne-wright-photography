d625c7ba41d7a135fe1af6b2b21a84d0d437ac5a
alastairkitchen <ali7566@gmail.com>
2020-09-05 14:24:26 +0100

    adds about graphQL function

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,15 +3,18 @@ import SiteLayout from "../components/layout/siteLayout";
 
 export default class About extends React.Component {
   render() {
+
+    console.dir(this.props.data);
+
     return (
       <SiteLayout>
-        <h1 class="content-h1">About</h1>
-        <div class="about-grid">
-          <article class="about-grid__image-col">
+        <h1 className="content-h1">About</h1>
+        <div className="about-grid">
+          <article className="about-grid__image-col">
             <img src="/images/about-me-portrait.jpg" />
           </article>
-          <div class="about-grid__text-col">
-            <div class="about-grid__text-inner ">
+          <div className="about-grid__text-col">
+            <div className="about-grid__text-inner ">
               <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, ventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore uatur? Quis autem vel eum iure reprehenderit qui in ea voluptate</p>
               <p>et quasi architecto beatae vitae dicta sunt explicabun exercitationem ullam vel eum iure reprehenderit qui in ea voluptate</p>
               <p>Sed ut perspiciatis unde omnis iste naqui ratione voluptatem sequi nesciunt. Nincidunt ut labore uatur? Quis autem vel eum iure reprehenderit qui in ea voluptate</p>
@@ -22,3 +25,28 @@ export default class About extends React.Component {
     );
   }
 }
+
+export const query = graphql`
+  query {
+    pageData: allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: { eq: "about-page" }
+          title: { eq: "About" }
+        }
+      }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            templateKey
+            description
+            mainContent
+          }
+        }
+      }
+    }
+  }
+`;
