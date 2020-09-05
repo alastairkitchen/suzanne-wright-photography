e089f994adc48a97d1b9c5f947602f4db5655ec1
alastairkitchen <ali7566@gmail.com>
2020-09-05 15:23:02 +0100

    adds graph QL function to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,23 @@
 import React from "react";
 import SiteLayout from "../components/layout/siteLayout";
+import { extractFrontMatterData } from "../utils/graphqlUtil";
 
 export default class Contact extends React.Component {
   render() {
+
+    const content = extractFrontMatterData(this.props.data.pageData);
+    console.dir(content);
+
     return (
       <SiteLayout>
-        <div class="contact">
-          <h1 class="content-h1">Contact</h1>
+        <div className="contact">
+          <h1 className="content-h1">Contact</h1>
+          {content.mainContent}
           <p>Email: <a href="mailto:suzemailaddress@test.com">suzemailaddress@test.com</a></p>
           <p>Hello lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
-          <div class="table">
-            <p class="table__header">Pricing:</p>
-            <ul class="table__content">
+          <div className="table">
+            <p className="table__header">Pricing:</p>
+            <ul className="table__content">
               <li>package 1 £xxx</li>
               <li>package 2 £xxx</li>
               <li>package 3 £xxx</li>
@@ -23,3 +29,29 @@ export default class Contact extends React.Component {
     );
   }
 }
+
+export const query = graphql`
+  query {
+    pageData: allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: { eq: "contact-page" }
+          title: { eq: "Contact" }
+        }
+      }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            templateKey
+            description
+            mainContent
+            mainPricing
+          }
+        }
+      }
+    }
+  }
+`;
