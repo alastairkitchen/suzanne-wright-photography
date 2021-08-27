4fdb3622d7e59897c92700e5b79a21d3198b6fc1
alastairkitchen <ali7566@gmail.com>
2021-08-27 14:54:04 +0100

    adds favicon

--- src/html.js
+++ src/html.js
@@ -5,6 +5,10 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
